b36411588c5631c666c4e1e91a72ec8187f8f38b
Jeremy Jackson <jj.cseng@gmail.com>
2017-11-30 23:45:20 +0530

    2nd Commit

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
 package com.TW;
 
 public class Main {
-
+    private String constructString(int n, String x){
+        StringBuilder fullstring = new StringBuilder("");
+        fullstring.ensureCapacity(n);
+        for(int i=0; i<n; i++) fullstring.append(x);
+        return fullstring.toString();
+    }
+    private void ex1(){
+        System.out.println("*");
+    }
+    private void ex2(int n){
+        String answer = constructString(n,"*");
+        System.out.println(answer);
+    }
     public static void main(String[] args) {
 
     }
