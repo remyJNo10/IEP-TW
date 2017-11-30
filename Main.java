64f758032b3db75c5a3105601c23c803ff87783d
Jeremy Jackson <jj.cseng@gmail.com>
2017-12-01 00:19:36 +0530

    3rd Commit

--- Main.java
+++ Main.java
@@ -1,20 +1,41 @@
 package com.TW;
 
 public class Main {
-    private String constructString(int n, String x){
+    static private String ntimes(int n, String x){
+
         StringBuilder fullstring = new StringBuilder("");
         fullstring.ensureCapacity(n);
         for(int i=0; i<n; i++) fullstring.append(x);
         return fullstring.toString();
     }
-    private void ex1(){
+    static private void ex1(){
         System.out.println("*");
     }
-    private void ex2(int n){
-        String answer = constructString(n,"*");
+    static private void ex2(int n){
+        String answer = ntimes(n,"*");
         System.out.println(answer);
     }
+    static private void ex3(int n){
+        StringBuilder answer= new StringBuilder("");
+        for(int i=0; i<n; i++){
+            answer.append("*\n");
+        }
+        System.out.println(answer.toString());
+    }
+    static private void ex4(int n){
+        StringBuilder answer = new StringBuilder("");
+        for(int i=1; i<=n; i++) answer.append(ntimes(i, "*")).append("\n");
+        System.out.println(answer.toString());
+    }
+    static private void ex5(int n){
+        StringBuilder answer = new StringBuilder("");
+        for(int i=1; i<=n; i++){
+//            assert (n-i) > 0;
+            answer.append(ntimes(n-i," ")).append(ntimes(2*i-1,"*")).append("\n");
+        }
+        System.out.println(answer.toString());
+    }
     public static void main(String[] args) {
-
+        ex5(5);
     }
 }
