795419071397af78bd2fe6f9a54eeeeaabe7dde6
Jeremy Jackson <jj.cseng@gmail.com>
2017-12-01 00:57:04 +0530

    added FizzBuzz

--- IEP.java
+++ IEP.java
@@ -54,7 +54,7 @@ public class IEP {
 //            assert (n-i) > 0;
             answer.append(ntimes(n-i," ")).append(ntimes(2*i-1,"*")).append("\n");
         }
-        System.out.println((n-name.length())/2);
+//        System.out.println((n-name.length())/2);
         answer.append(ntimes((2*n-1-name.length())/2," ")).append(name).append("\n");
         for(int i=n-1; i>=1; i--){
 //            assert (n-i) > 0;
@@ -63,8 +63,44 @@ public class IEP {
         System.out.println(answer.toString());
 
     }
+    static private void fizzBuzz(){
+        for (int i = 1; i <= 100; i++) {
+            if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i%3==0){
+                System.out.println("Fizz");
+            }
+            else if(i%5==0){
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+    static private void fizzBuzzAlt(){
+        for (int i = 1; i <= 100; i++) {
+            String s = "";
+            if(i%3==0){
+                s += "Fizz";
+            }
+            if(i%5==0){
+                s += "Buzz";
+            }
+            if(s.length()>0){
+                System.out.println(s);
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+
     public static void main(String[] args) {
 //        ex7(6,"Billy");
 //        ex6(5);
+        fizzBuzzAlt();
+
     }
 }
